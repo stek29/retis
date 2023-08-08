e7e8cc3d361be85ac1cfb4a32d3eb1080ac31c50
Paolo Valerio <pvalerio@redhat.com>
2023-08-08 12:03:04 +0200

    filters: intruduce register_filter()
    This will allow to globally register a new filter for later retrieval
    and inlining.
    
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- src/core/filters/filters.rs
+++ src/core/filters/filters.rs
@@ -1,8 +1,27 @@
 /// eBPF filter wrapper containing the sequence of bytes composing the eBPF program
+
+use std::{collections::HashMap, sync::Mutex};
+
+use anyhow::Result;
+use once_cell::sync::Lazy;
+
 #[derive(Clone)]
 pub(crate) struct BpfFilter(pub(crate) Vec<u8>);
 
 #[derive(Clone)]
 pub(crate) enum Filter {
     Packet(BpfFilter),
+}
+
+static FM: Lazy<Mutex<HashMap<u32, Filter>>> = Lazy::new(|| {
+    Mutex::new(HashMap::new())
+});
+
+pub(crate) fn register_filter(r#type: u32, filter: &Filter) -> Result<()> {
+    FM.lock().unwrap().insert(r#type, filter.clone());
+    Ok(())
+}
+
+pub(crate) fn get_filter(r#type: u32) -> Option<Filter> {
+    FM.lock().unwrap().get(&r#type).cloned()
 }
